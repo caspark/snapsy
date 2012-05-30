518b40e5618bda48e9050e1b3c2075d2bd41762b
Caspar Krieger <caspar.krieger@gmail.com>
2012-05-30 23:34:25 +0800

    Initial commit with code from https://naps.svn.sourceforge.net/svnroot/naps/

---
+++ FPDFSave.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NAPS
+{
+    public partial class FPDFSave : Form
+    {
+        public FPDFSave()
+        {
+            InitializeComponent();
+        }
+
+        public void SetStatus(int count, int total)
+        {
+            lblStatus.Text = count.ToString() + " of " + total.ToString();
+            Application.DoEvents();
+        }
+    }
+
+}
